a841e239d85e7e3914318539176ac021b4d24ac6
SorokinMichail <91275394+SorokinMichail@users.noreply.github.com>
2021-10-15 16:48:08 +0300

    Update program.c

--- program.c
+++ program.c
@@ -4,12 +4,12 @@
 #include <stdbool.h>
 #include <ctype.h>
 
-void cheker(int **A, bool *c, int n)
+void cheker(int** A, bool* c, int n, int max)
 {
-	for (int i = n; i > 0; i--) {
+	for (int i = max; i > 0; i--) {
 		if (A[n][i] == 1 && c[i] == false) {
 			c[i] = true;
-			cheker(A, c, i);
+			cheker(A, c, i, max);
 		}
 	}
 }
@@ -17,9 +17,9 @@ void cheker(int **A, bool *c, int n)
 int main()
 {
 	int length = 0;
-	int **matrix = (int **)malloc(sizeof(int *));
-	FILE *graf;
-	char *fname = "gr.gv";
+	int** matrix = (int**)malloc(sizeof(int*));
+	FILE* graf;
+	char* fname = "gr.gv";
 	graf = fopen(fname, "w");
 	fprintf(graf, "digraph g{");
 	printf("select graph: \n");
@@ -39,12 +39,15 @@ int main()
 			else {
 				if (boofer - '0' > length || c - '0' > length) {
 					int n = max(boofer - '0', c - '0');
-					matrix = (int **)realloc(matrix, (n + 1) * sizeof(int*));
+					matrix = (int**)realloc(matrix, (n + 1) * sizeof(int*));
 					for (int i = length + 1; i <= n; i++) {
-						matrix[i] = (int *)malloc(sizeof(int));
+						matrix[i] = (int*)malloc(2*sizeof(int));
 					}
 					for (int i = 1; i <= n; i++) {
-						matrix[i] = (int *)realloc(matrix[i], n*sizeof(int));
+						matrix[i] = (int*)realloc(matrix[i], (n+1) * sizeof(int));
+						for (int j = 1; j <= n; j++) {
+							if (matrix[i][j] != 1)matrix[i][j] = 0;
+						}
 					}
 
 					length = n;
@@ -57,21 +60,16 @@ int main()
 	}
 	fprintf(graf, "}");
 	fclose(graf);
-		for (int i = 1; i <= length; i++) {
-		for (int j = 1; j <= length; j++) {
-			printf("%*d", 5, matrix[i][j]);
-		}
-		printf("\n");
-	}
-	bool *chek = (bool *)malloc((length + 1) * sizeof(bool));
+	bool* chek = (bool*)malloc((length + 1) * sizeof(bool));
 	for (int i = 1; i < length; i++)chek[i] = false;
 	chek[length] = true;
-	cheker(matrix, chek, length);
+	cheker(matrix, chek, length,length);
 	bool f = true;
 	for (int i = 1; i <= length; i++)
 		if (!chek[i])f = false;
 	if (f) printf("True");
 	else printf("False");
-	//system("dot -Tpng gr.gv -o gr.png");
+	system("dot -Tpng gr.gv -o gr.png");
+	system("gr.png");
 	return 0;
 }

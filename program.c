19a11d394e38c6da998a3cb4405e2c890e3d8955
SorokinMichail <91275394+SorokinMichail@users.noreply.github.com>
2021-12-10 12:08:52 +0300

    Update program.c

--- program.c
+++ program.c
@@ -3,8 +3,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <ctype.h>
-
-
 void cheker(int** A, bool* c, int n, int max)
 {
 	for (int i = max; i > 0; i--) {
@@ -14,80 +12,82 @@ void cheker(int** A, bool* c, int n, int max)
 		}
 	}
 }
-
 int max(int x, int y)
 {
 	return (x > y ? x : y);
 }
-
 int main()
 {
-	int length = 0;
-	int** matrix = (int**)malloc(sizeof(int*));
+	int** matrix = (int**)malloc(2 * sizeof(int*));
 	FILE* graf;
 	char fname[] = "gr.gv";
 	graf = fopen(fname, "w");
 	fprintf(graf, "graph g{");
 	printf("select graph: \n");
 	char c = getchar();
-	char boofer = '&';
+	int x = 1;
+	int y = 1;
+	int by = 0;
 	while (c != '|') {
-		if (c == '\n')
-		{
-			fprintf(graf, ";");
-			if (boofer - '0' > length) {
-				int n = boofer;
-				matrix = (int**)realloc(matrix, (n + 1) * sizeof(int*));
-				for (int i = length + 1; i <= n; i++) {
-					matrix[i] = (int*)malloc(2 * sizeof(int));
-				}
-				for (int i = 1; i <= n; i++) {
-					matrix[i] = (int*)realloc(matrix[i], (n + 1) * sizeof(int));
-					for (int j = 1; j <= n; j++) {
-						if (matrix[i][j] != 1)matrix[i][j] = 0;
-					}
-				}
-
-				length = n;
-				boofer = '&';
-
+		matrix[x] = (int*)malloc(by * sizeof(int));
+		while (c != '\n' && c!= '|') {
+			if (y > by)
+			{
+				matrix[x] = (int*)realloc(matrix[x], (y + 1) * sizeof(int));
+				by = y;
+			}
+			matrix[x][y] = c - '0';
+			y++;
+			c = getchar();
+		}
+		y = 1;
+		if (c == '|')break;
+		x++;
+		c = getchar();
+		matrix = (int**)realloc(matrix, (x + 1) * sizeof(int*));
+	}
+	int** matrix2 = (int**)malloc((x+1) * sizeof(int*));
+	for (int i = 1; i <= x; i++) {
+		matrix2[i] = (int*)malloc((x+1) * sizeof(int));
+		for (int j = 1; j <= x; j++) {
+			matrix2[i][j] = 0;
+		}
+	}
+	for (int i = 1; i <= by; i++) {
+		int b1 = 0;
+		int b2 = 0;
+		bool f = false;
+		for (int j = 1; j <= x; j++) {
+			if (matrix[j][i] == 1) {
+				if (f) b2 = j;
+				else b1 = j;
+				f = true;
 			}
 		}
-		fprintf(graf, "%c", c);
-		if (isdigit(c))
-			if (boofer == '&') {
-				boofer = c;
-			}
+		if (b1 != 0) {
+			if (b2 == 0)fprintf(graf, "%d--%d;\n", b1,b1);
 			else {
-				if (boofer - '0' > length || c - '0' > length) {
-					int n = max(boofer - '0', c - '0');
-					matrix = (int**)realloc(matrix, (n + 1) * sizeof(int*));
-					for (int i = length + 1; i <= n; i++) {
-						matrix[i] = (int*)malloc(2 * sizeof(int));
-					}
-					for (int i = 1; i <= n; i++) {
-						matrix[i] = (int*)realloc(matrix[i], (n + 1) * sizeof(int));
-						for (int j = 1; j <= n; j++) {
-							if (matrix[i][j] != 1)matrix[i][j] = 0;
-						}
-					}
-
-					length = n;
-				}
-				matrix[boofer - '0'][c - '0'] = 1;
-				matrix[c - '0'][boofer - '0'] = 1;
-				boofer = '&';
+				fprintf(graf, "%d--%d;\n", b1, b2);
+				matrix2[b1][b2] = 1;
+				matrix2[b2][b1] = 1;
 			}
-		c = getchar();
+		}
+	}
+	for (int i = 1; i <= x; i++) {
+		float c=false;
+		for (int j = 1; j <= by; j++) {
+			if (matrix[i][j] == 1)c = true;
+		}
+		if(!c)fprintf(graf, "%d;\n", i);
 	}
 	fprintf(graf, "}");
 	fclose(graf);
-	bool* chek = (bool*)malloc((length + 1) * sizeof(bool));
-	for (int i = 1; i < length; i++)chek[i] = false;
+	bool* chek = (bool*)malloc((x + 1) * sizeof(bool));
+	for (int i = 1; i <= x; i++)chek[i] = false;
 	chek[1] = true;
-	cheker(matrix, chek, 1, length);
+	cheker(matrix2, chek, 1, x);
 	bool f = true;
-	for (int i = 1; i <= length + 1; i++)
+	for (int i = 1; i <= x; i++)
 		if (!chek[i])f = false;
 	if (f) printf("True");
 	else printf("False");
